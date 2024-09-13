287c7ed6b3451f16d778c6fb5234a7617a3fd58c
m0nkai <arsen.makaov@gmail.com>
2024-09-14 00:27:40 +0300

    first

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var input int
+	fmt.Scan(&input)
+
+	str := strconv.Itoa(input)
+	result := ""
+
+	for _, char := range str {
+		num, _ := strconv.Atoi(string(char))
+		square := num * num
+		result += strconv.Itoa(square)
+	}
+
+	fmt.Println(result)
 }
